5256876ec70403c2b5cd509d231dc85a14e40e83
ronaldsrundans <ronaldsrundans@gmail.com>
2020-10-07 11:52:49 +0300

    Created new file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CV saraksts</title>
+</head>
+<body>
+    <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                </div>
+            </div>  
+    </div>        
+</body>    
+</html>
