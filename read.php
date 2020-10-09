69e0a564ca01f4293b28992d93dd38b47fe9a5e9
ronaldsrundans <ronaldsrundans@gmail.com>
2020-10-09 12:27:59 +0300

    Added file read.php and it selects id row from DB.

---
+++ read.php
@@ -0,0 +1,91 @@
+<?php
+
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM Persons WHERE id = ?";
+    
+    if($stmt = $conn->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bind_param("i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+            
+            if($result->num_rows == 1){
+                /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $Fname = $row["Fname"];
+                $Email = $row["Email"];
+                $Phone = $row["Phone"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    $stmt->close();
+    
+    // Close connection
+    $conn->close();
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View CV Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>View CV Record</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>First name</label>
+                        <p class="form-control-static"><?php echo $row["Fname"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <p class="form-control-static"><?php echo $row["Email"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Phone</label>
+                        <p class="form-control-static"><?php echo $row["Phone"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
