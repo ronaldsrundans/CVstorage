cbedd8836dad8fbd08d693ae382cc98032f43667
ronaldsrundans <ronaldsrundans@gmail.com>
2020-10-09 14:23:48 +0300

    Added file update.php .

---
+++ update.php
@@ -0,0 +1,187 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$firstname = $lastname = $email = $phone = "";
+$firstname_err = $lastname_err = $email_err = $phone_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Get hidden input value
+    $id = $_POST["id"];
+    
+    // Validate name
+    $input_firstname = trim($_POST["firstname"]);
+    if(empty($input_firstname)){
+        $firstname_err = "Please enter a first name.";
+    } elseif(!filter_var($input_firstname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $firstname_err = "Please enter a valid first name.";
+    } else{
+        $firstname = $input_firstname;
+    }
+
+    $input_lastname = trim($_POST["firstname"]);
+    if(empty($input_lastname)){
+        $lastname_err = "Please enter a first name.";
+    } elseif(!filter_var($input_lastname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $lastname_err = "Please enter a valid first name.";
+    } else{
+        $lastname = $input_lastname;
+    }
+    
+    // Validate address address
+    $input_address = trim($_POST["email"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an email.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate salary
+    $input_salary = trim($_POST["phone"]);
+    if(empty($input_salary)){
+        $salary_err = "Please enter the phone number.";     
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Please enter a phone number.";
+    } else{
+        $salary = $input_salary;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($firstname_err) && empty($lastname_err) && empty($email_err) && empty($phone_err)){
+        // Prepare an update statement
+        $sql = "UPDATE Persons SET firstname=?, lastname=?, email=?, phone=? WHERE id=?";
+         
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssssi", $param_firstname, $param_lastname, $param_email, $param_phone, $param_id);
+            
+            // Set parameters
+            $param_firstname = $firstname;
+            $param_lastname = $lastname;
+            $param_email = $email;
+            $param_phone = $phone;
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM Persons WHERE id = ?";
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            
+            // Set parameters
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $firstname = $row["firstname"];
+                    $lastname = $row["lastname"];
+                    $email = $row["email"];
+                    $phone = $row["phone"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($conn);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update CV Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Update CV Record</h2>
+                    </div>
+                    <p>Please edit the input values and submit to update the record.</p>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($firstname_err)) ? 'has-error' : ''; ?>">
+                            <label>First Name</label>
+                            <input type="text" name="firstname" class="form-control" value="<?php echo $firstname; ?>">
+                            <span class="help-block"><?php echo $firstname_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($lastname_err)) ? 'has-error' : ''; ?>">
+                            <label>Last Name</label>
+                            <input type="text" name="lastname" class="form-control" value="<?php echo $lastname; ?>">
+                            <span class="help-block"><?php echo $firstname_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                            <label>Email</label>
+                            <textarea name="email" class="form-control"><?php echo $email; ?></textarea>
+                            <span class="help-block"><?php echo $email_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($phone_err)) ? 'has-error' : ''; ?>">
+                            <label>Phone</label>
+                            <input type="text" name="phone" class="form-control" value="<?php echo $phone; ?>">
+                            <span class="help-block"><?php echo $phone_err;?></span>
+                        </div>
+                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
